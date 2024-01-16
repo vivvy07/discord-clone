a4a88fce74c2604d1261f74710f720a4dc6b8fa1
Vivek Kumar <vkhp77@gmail.com>
2024-01-17 01:32:54 +0530

    delete and leave server

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -68,6 +68,7 @@ export const ServerHeader = ({ server, role }: ServerHeaderProps) => {
         )}
         {isAdmin && (
             <DropdownMenuItem
+            onClick={() => onOpen("deleteServer", { server })}
             className="text-rose-500 px-3 py-2 text-sm cursor-pointer"
             >
             Delete Server
@@ -76,6 +77,7 @@ export const ServerHeader = ({ server, role }: ServerHeaderProps) => {
         )}
         {!isAdmin && (
             <DropdownMenuItem
+            onClick={() => onOpen("leaveServer", { server })}
             className="text-rose-500 px-3 py-2 text-sm cursor-pointer"
             >
             Leave Server
