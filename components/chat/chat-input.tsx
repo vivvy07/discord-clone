a85b433a693ca69a7a5ba0b36edcc9ab961614e3
Vivek Kumar <vkhp77@gmail.com>
2024-01-23 17:43:20 +0530

    images and pdf upload

--- components/chat/chat-input.tsx
+++ components/chat/chat-input.tsx
@@ -8,6 +8,7 @@ import { Form, FormControl, FormField, FormItem } from "../ui/form";
 import { Input } from "../ui/input";
 import { Plus, Smile } from "lucide-react";
 import axios from "axios";
+import { useModal } from "@/hooks/use-modal-store";
 
 interface ChatInputProps {
     apiUrl: string;
@@ -26,6 +27,7 @@ export const ChatInput = ({
     name,
     type,
 }: ChatInputProps) => {
+    const { onOpen } = useModal();
     const form = useForm<z.infer<typeof formSchema>>({
         resolver: zodResolver(formSchema),
         defaultValues: {
@@ -60,7 +62,7 @@ export const ChatInput = ({
                             <div className="relative p-4 pb-6">
                                 <button
                                 type="button"
-                                onClick={() => {}}
+                                onClick={() => onOpen("messageFile", { apiUrl, query })}
                                 className="absolute top-7 left-8 h-[24px] w-[24px]
                                 bg-zinc-500 dark:bg-zinc-400 hover:bg-zinc-600 
                                 dark:hover:bg-zinc-300 transition rounded-full p-1 flex 
