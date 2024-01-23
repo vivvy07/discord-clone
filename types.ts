6bfa2b539dd713235df108c38853afa6e719684d
Vivek Kumar <vkhp77@gmail.com>
2024-01-23 17:08:15 +0530

    api for messages

--- types.ts
+++ types.ts
@@ -1,5 +1,5 @@
 import { Server as NetServer, Socket } from "net";
-import { NextApiRequest } from "next";
+import { NextApiResponse } from "next";
 import { Server as SocketIOServer } from "socket.io";
 import { Member, Profile, Server } from "@prisma/client";
 
@@ -7,7 +7,7 @@ export type ServerWithMembersWithProfiles = Server & {
     members: (Member & { profile: Profile})[];
 }
 
-export type NextApiResponseServerIo = NextApiRequest & {
+export type NextApiResponseServerIo = NextApiResponse & {
     socket: Socket & {
         server: NetServer & {
             io: SocketIOServer
