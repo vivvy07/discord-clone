90e89411a7d27fb827fec816adf507be73b85eb1
Vivek Kumar <vkhp77@gmail.com>
2024-02-09 16:48:58 +0530

    shifted to postgresql

--- lib/db.ts
+++ lib/db.ts
@@ -1,9 +1,19 @@
-import { PrismaClient } from "@prisma/client";
+import { Pool } from 'pg'
+import { PrismaPg } from '@prisma/adapter-pg'
+import { PrismaClient } from '@prisma/client'
+
+const connectionString = `${process.env.DATABASE_URL}`
+
+const pool = new Pool({ connectionString })
+const adapter = new PrismaPg(pool)
 
 declare global {
     var prisma: PrismaClient | undefined;
 };
 
-export const db = globalThis.prisma || new PrismaClient();
+// Pass the adapter option to the Prisma Client instance
+// Pass the adapter option to the Prisma Client instance
+
+export const db = globalThis.prisma || new PrismaClient({adapter});
 
 if (process.env.NODE_ENV !== "production") globalThis.prisma = db
--- lib/initial-profile.ts
+++ lib/initial-profile.ts
@@ -1,6 +1,5 @@
 import { currentUser, redirectToSignIn } from "@clerk/nextjs";
-
-import { db } from "./db";
+import {db} from "./db";
 
 export const initialProfile =async () => {
     const user = await currentUser();
